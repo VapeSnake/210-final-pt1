e2601723b7b9c412e32101d27671ce4c0e4c767c
Noel Mier-Luna <noel.lmier@gmail.com>
2026-05-20 19:25:10 -0700

    2nd Commit

---
+++ 210_final_pt1.cpp
@@ -0,0 +1,35 @@
+// COMSC_210 | Final Pt 1 | Noel Mier-Luna
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    map<string, int> flights;
+
+    ifstream inFile;
+    inFile.open("data.txt");
+    if (!inFile) {
+        cerr << "Unable to open file data.txt";
+        return 1; // Exit with error code
+    }
+    string flight;
+    int count;
+
+    while (inFile >> flight) {
+        if (flights.find(flight) == flights.end()) {
+            flights[flight] = 1;
+        } else {
+            flights[flight]++;
+        }
+        
+    }
+    for (const auto& pair : flights) {
+        cout << pair.first << ": " << pair.second << endl;
+    }
+
+}
